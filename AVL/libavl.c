3194fdaa16e00cf1997d9301a7295abf36ae3211
LucasMull <muller.lucas@Hotmail.com>
2020-12-15 17:45:04 -0300

    refactor: renomear variável, formatação

--- AVL/libavl.c
+++ AVL/libavl.c
@@ -1,10 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
 #include "libavl.h"
 
-nodo_t *inicia_nodo(int chave) {
-    nodo_t *n = (nodo_t*)malloc(sizeof(nodo_t));
-    n->chave = chave;
-    n->esq = NULL;
-    n->dir = NULL;
-    n->pai = NULL;
-    return n;
+nodo_t*
+inicia_nodo(int chave)
+{
+    nodo_t *novo_nodo = malloc(sizeof *novo_nodo);
+    assert(NULL != novo_nodo); /* verifica se memória foi alocada para novo nodo */
+
+    novo_nodo->esq = NULL;
+    novo_nodo->dir = NULL;
+    novo_nodo->pai = NULL;
+
+    novo_nodo->chave = chave;
+    novo_nodo->altura = 1;
+
+    return novo_nodo;
 }
