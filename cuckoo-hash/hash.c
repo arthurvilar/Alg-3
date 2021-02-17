e91e21fa6eeb5054cda9869f23ecd37a2fba425a
arthurvilar <ahc.vilar@gmail.com>
2021-02-17 01:11:50 -0300

    feat: delete function

--- cuckoo-hash/hash.c
+++ cuckoo-hash/hash.c
@@ -2,7 +2,7 @@
 
 void initTable(int t[]) {
     for (int i = 0; i < SIZE; i++)
-        t[i] = -1;  //posição livre
+        t[i] = FREE;  // posição livre
 }
 
 int h1(int key) {
@@ -24,31 +24,31 @@ int h2(int key) {
 void insert(int key, int t1[], int t2[]) {
     int pos1 = h1(key);
     
-    //inserção na tabela 1
+    // inserção na t1
     if (t1[pos1] == -1 || t1[pos1] == -2) {
         t1[pos1] = key;
     }
-    else if (h1(t1[pos1]) == pos1) {    //colisão em T1
+    else if (h1(t1[pos1]) == pos1) {    // colisão em T1
         int temp = t1[pos1];
         
-        if (key == t1[pos1]) {      //se chave nova e antiga sao iguais ignora
+        if (key == t1[pos1]) {      // se chave nova e antiga sao iguais ignora
             return;
-        } else {
+        } else {                    // inserção na t2
             t1[pos1] = key;
             t2[h2(temp)] = temp;
         }
+    }
+}
 
-        /*
-        Implementação 2 (sem usar temp):
-        
-        int temp = t1[pos1];
+void delete (int key, int t1[], int t2[]) {
+    int pos1 = h1(key);
+    int pos2 = h2(key);
 
-        if (key == t1[pos1]) {
-            return;
-        } else {
-            t2[h2(t1[pos1])] = t1[pos1];
-            t1[pos1] = key;
-        }
-        */
+    if (t1[pos1] == key) {          // confere se o valor ta na t1
+        t1[pos1] = DEL;
+    }
+    else if (t1[pos1] != FREE) {
+        if (t2[pos2] == key)        // confere se o valor ta na t2
+            t2[pos2] = DEL;
     }
 }
--- cuckoo-hash/hash.h
+++ cuckoo-hash/hash.h
@@ -2,12 +2,17 @@
 #define __HASH__
 
 #define SIZE 11
+#define FREE -1     // valor inicial, espaço ainda não usado
+#define DEL -2      // valor para espaço que teve chave removida
 
 void initTable(int t[]);
 
 int h1(int key);
+
 int h2(int key);
 
 void insert(int key, int t1[], int t2[]);
+
+void delete (int key, int t1[], int t2[]);
 
 #endif
