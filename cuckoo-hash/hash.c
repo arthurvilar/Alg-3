d523814cfcfe5c51487d08059a1b8d6cccba5636
LucasMull <muller.lucas@Hotmail.com>
2021-02-18 11:01:30 -0300

    fix: erro burrito

--- cuckoo-hash/hash.c
+++ cuckoo-hash/hash.c
@@ -120,14 +120,19 @@ delete(int key, hashtable_t ht)
 void 
 print_ht(hashtable_t ht)
 {
-    for (size_t i = 0; i < ht.size; ++i) 
+    size_t i;
+
+    // Tabela 1
+    for (i = 0; i < ht.size; ++i) 
     {
-        // Tabela 1
         if (ht.t1[i] != FREE && ht.t1[i] != DEL) {
             printf("%d,T1,%ld\n", ht.t1[i], i);
         }
+    }
 
-        // Tabela 2
+    // Tabela 2
+    for (i = 0; i < ht.size; ++i) 
+    {
         if (ht.t2[i] != FREE && ht.t2[i] != DEL) {
             printf("%d,T2,%ld\n", ht.t2[i], i);
         }
